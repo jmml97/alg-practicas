d74e7094ad5b463e8c9949db3d2c176a222651d7
José María Martín Luque <jmmartinluque@gmail.com>
2017-03-16 19:35:27 +0100

    Cambiados todos los nombres de algoritmos a mayúsculas

--- p1/ajustes.sh
+++ p1/ajustes.sh
@@ -36,21 +36,21 @@ i(x) = d0*2**(x*d1)+d2
 fit f(x) "./datos/burbuja_datos/burbuja-linux-O0.dat" via a0,a1,a2
 
 set output "./graficos/ajuste-burbuja.tex"
-set title "Ajuste burbuja" tc '#4D4D4D'
+set title "Ajuste Burbuja" tc '#4D4D4D'
 
 plot f(x) with lines linestyle 3 title "Burbuja ajustada", "./datos/burbuja_datos/burbuja-linux-O0.dat" with points linestyle 1 pt 7 title "Burbuja"
 
 fit f(x) "./datos/insercion_datos/insercion-linux-O0.dat" via a0,a1,a2
 
 set output "./graficos/ajuste-insercion.tex"
-set title "Ajuste inserción" tc '#4D4D4D'
+set title "Ajuste Inserción" tc '#4D4D4D'
 
 plot f(x) with lines linestyle 3 title "Inserción ajustado", "./datos/insercion_datos/insercion-linux-O0.dat" with points linestyle 1 pt 7 title "Inserción"
 
 fit f(x) "./datos/seleccion_datos/seleccion-linux-O0.dat" via a0,a1,a2
 
 set output "./graficos/ajuste-seleccion.tex"
-set title "Ajuste selección" tc '#4D4D4D'
+set title "Ajuste Selección" tc '#4D4D4D'
 
 plot f(x) with lines linestyle 3 title "Selección ajustado", "./datos/seleccion_datos/seleccion-linux-O0.dat" with points linestyle 1 pt 7 title "Selección"
 
@@ -59,7 +59,7 @@ plot f(x) with lines linestyle 3 title "Selección ajustado", "./datos/seleccion
 fit g(x) "./datos/mergesort_datos/mergesort-linux-O0.dat" via b0,b1,b2
 
 set output "./graficos/ajuste-mergesort.tex"
-set title "Ajuste mergesort" tc '#4D4D4D'
+set title "Ajuste Mergesort" tc '#4D4D4D'
 set logscale y
 
 plot g(x) with lines linestyle 3 title "Mergesort ajustado", "./datos/mergesort_datos/mergesort-linux-O0.dat" with points linestyle 1 pt 7 title "Mergesort"
@@ -67,21 +67,21 @@ plot g(x) with lines linestyle 3 title "Mergesort ajustado", "./datos/mergesort_
 fit g(x) "./datos/heapsort_datos/heapsort-linux-O0.dat" via b0,b1,b2
 
 set output "./graficos/ajuste-heapsort.tex"
-set title "Ajuste heapsort" tc '#4D4D4D'
+set title "Ajuste Heapsort" tc '#4D4D4D'
 
 plot g(x) with lines linestyle 3 title "Heapsort ajustado", "./datos/heapsort_datos/heapsort-linux-O0.dat" with points linestyle 1 pt 7 title "Heapsort"
 
 fit g(x) "./datos/quicksort_datos/quicksort-linux-O0.dat" via b0,b1,b2
 
 set output "./graficos/ajuste-quicksort.tex"
-set title "Ajuste quicksort" tc '#4D4D4D'
+set title "Ajuste Quicksort" tc '#4D4D4D'
 
 plot g(x) with lines linestyle 3 title "Quicksort ajustado", "./datos/quicksort_datos/quicksort-linux-O0.dat" with points linestyle 1 pt 7 title "Quicksort"
 
 fit h(x) "./datos/floyd_datos/floyd-linux-O0.dat" via c0,c1,c2,c3
 
 set output "./graficos/ajuste-floyd.tex"
-set title "Ajuste floyd" tc '#4D4D4D'
+set title "Ajuste Floyd" tc '#4D4D4D'
 unset logscale y
 
 plot h(x) with lines linestyle 3 title "Floyd ajustado", "./datos/floyd_datos/floyd-linux-O0.dat" with points linestyle 1 pt 7 title "Floyd"
@@ -89,7 +89,7 @@ plot h(x) with lines linestyle 3 title "Floyd ajustado", "./datos/floyd_datos/fl
 fit i(x) "./datos/hanoi_datos/hanoi-linux-O0.dat" via d0, d1,d2
 
 set output "./graficos/ajuste-hanoi.tex"
-set title "Ajuste hanoi" tc '#4D4D4D'
+set title "Ajuste Hanoi" tc '#4D4D4D'
 
 plot i(x) with lines linestyle 3 title "Hanoi ajustado", "./datos/hanoi_datos/hanoi-linux-O0.dat" with points linestyle 1 pt 7 title "Hanoi"
 
