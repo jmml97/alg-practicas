78ac99e7688ac98eee7dad3185a547a589c26d92
antcc <antoniocoin07@gmail.com>
2017-03-23 22:46:21 +0100

    Añadidos ajustes malos

--- p1/ajustes.sh
+++ p1/ajustes.sh
@@ -29,7 +29,7 @@ set ylabel "Tiempo de ejecución (s)" tc '#4D4D4D' offset 2,0
 set xtics rotate by 45 right font ", 8"
 
 f(x) = a0*x*x+a1*x+a2
-g(x) = x*b0+x*b1*log(x) + b2
+g(x) = b0*x*log(x) + b1*x + b2
 h(x) = c0*x*x*x+c1*x*x+c2*x+c3
 i(x) = d0*2**(x*d1)+d2
 
@@ -66,21 +66,21 @@ set output "./graficos/ajuste-mergesort.tex"
 set title "Ajuste Mergesort" tc '#4D4D4D'
 
 
-plot g(x) with lines linestyle 3 title sprintf("%2.2e\$ \\\cdot x^{%2.2e}+x \\\cdot %2.2e\$", b0, b1, b2), "./datos/mergesort_datos/mergesort-linux-O0.dat" with points linestyle 1 pt 7 title "Mergesort"
+plot g(x) with lines linestyle 3 title sprintf("\$ %2.2e \\\cdot x\\\log x + %2.2e x + %2.2e\$", b0, b1, b2), "./datos/mergesort_datos/mergesort-linux-O0.dat" with points linestyle 1 pt 7 title "Mergesort"
 
 fit g(x) "./datos/heapsort_datos/heapsort-linux-O0.dat" via b0,b1,b2
 
 set output "./graficos/ajuste-heapsort.tex"
 set title "Ajuste Heapsort" tc '#4D4D4D'
 
-plot g(x) with lines linestyle 3 title sprintf("%2.2e\$ \\\cdot x^{%2.2e}+x \\\cdot %2.2e\$", b0, b1, b2), "./datos/heapsort_datos/heapsort-linux-O0.dat" with points linestyle 1 pt 7 title "Heapsort"
+plot g(x) with lines linestyle 3 title sprintf("\$ %2.2e \\\cdot x\\\log x + %2.2e x + %2.2e\$", b0, b1, b2), "./datos/heapsort_datos/heapsort-linux-O0.dat" with points linestyle 1 pt 7 title "Heapsort"
 
 fit g(x) "./datos/quicksort_datos/quicksort-linux-O0.dat" via b0,b1,b2
 
 set output "./graficos/ajuste-quicksort.tex"
 set title "Ajuste Quicksort" tc '#4D4D4D'
 
-plot g(x) with lines linestyle 3 title sprintf("%2.2e\$ \\\cdot x^{%2.2e}+x \\\cdot %2.2e\$", b0, b1, b2), "./datos/quicksort_datos/quicksort-linux-O0.dat" with points linestyle 1 pt 7 title "Quicksort"
+plot g(x) with lines linestyle 3 title sprintf("\$ %2.2e \\\cdot x\\\log x + %2.2e x + %2.2e\$", b0, b1, b2), "./datos/quicksort_datos/quicksort-linux-O0.dat" with points linestyle 1 pt 7 title "Quicksort"
 
 fit h(x) "./datos/floyd_datos/floyd-linux-O0.dat" via c0,c1,c2,c3
 
