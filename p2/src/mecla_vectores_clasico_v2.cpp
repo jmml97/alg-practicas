ff31ddf1c4a9aa346251abd8a6f7a978551b6aac
Mike <miguel_lb_97@hotmail.com>
2017-04-06 12:12:30 +0200

    Update mecla_vectores_clasico_v2.cpp

--- p2/src/mecla_vectores_clasico_v2.cpp
+++ p2/src/mecla_vectores_clasico_v2.cpp
@@ -8,6 +8,41 @@ using namespace std;
 //#include <algorithm>
 //#include <vector>
 
+// Mezcla K vectores de N tamaño pasados por T y que se guardan en res
+// Cuidado, hay que crear res de tamaño N = nk
+void MezclarK(int** T, int* res, int n, int k)
+{
+  // Tamaño total del vector
+  int N = n * k;
+  
+  // Vector donde vamos a guardar el indice del valor que falta por meter de cada vector
+  int* v_indices = new int[k];
+
+  // Inicializamos al último indice de cada vector (los máximos)
+  for (int i = 0; i < k; ++i)
+    v_indices[i] = n - 1;
+
+  // El bucle dura hasta N = nk colocados; empezamos en N y acabamos en -1
+  int indice_colocar = N - 1;
+  while (indice_colocar >= 0)
+  {
+    // Este indice indica que vector es el que contiene el valor max.
+    int indice_max = 0;
+    // Recorremos buscando el indice que contenga el valor mayor
+    for (int i = 0; i < k; ++i)
+      if (T[indice_max][v_indices[indice_max]] < T[i][v_indices[i]])
+        indice_max = i;
+    // Guardamos en res con el valor correspondiente
+    res[indice_colocar] = T[indice_max][v_indices[indice_max]];
+    // Decrementamos el indice corresponiente al vector que acabamos de usar
+    --v_indices[indice_max];
+    // Hemos colocado uno
+    --indice_colocar;
+  }
+
+  delete [] v_indices;
+}
+
 //generador de ejemplor para el problema de mezcla de k vectores ordenados. Para obtener k vectores ordenados de forma creciente cada uno con n elementos, genera un vector de tamaño $k*n$ con todos los enteros entre 0 y kn-1 ordenados. Se lanzan entonces k iteraciones de un algoritmo de muestreo aleatorio de tamaño n para obtener los k vectores. Están ordeados porque el algoritmo de muestreo mantiene el orden
 
 double uniforme() //Genera un número uniformemente distribuido en el
@@ -69,26 +104,9 @@ int main(int argc, char * argv[])
    cout << " " << endl;
   }
 
-  int* v_indices = new int[k];
   int* res = new int[N];
-
-  for (int i = 0; i < k; ++i)
-    v_indices[i] = n - 1;
-
-  int indice_colocar = N - 1;
-  while (indice_colocar >= 0)
-  {
-    int indice_max = 0;
-    for (int i = 0; i < k; ++i)
-    // Mirar este if
-      if (T[indice_max][v_indices[indice_max]] < T[i][v_indices[i]])
-        indice_max = i;
-    res[indice_colocar] = T[indice_max][v_indices[indice_max]];
-    --v_indices[indice_max];
-    --indice_colocar;
-  }
-
-  delete [] v_indices;
+  mezclaK(T, res, n, k);
+  
   delete [] T;
 
   cout << "\nRes: ";
